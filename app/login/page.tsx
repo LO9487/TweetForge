4585ee91e02641088315f30a91790072b7210d74
LO9487 <lojkeng@gmail.com>
2024-05-17 23:25:07 +0800

    login and register settle

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+"use client"
+import React, { useEffect } from 'react';
 import Link from "next/link"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Button } from "@/components/ui/button"
-
+import { useRouter } from 'next/navigation';
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const LoginPage = () => {
+  const router = useRouter();
+
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const onLogin = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/mainpage");
+      
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if(user.email.length > 0 && user.password.length > 0) {
+        setButtonDisabled(false);
+    } else{
+        setButtonDisabled(true);
+    }
+}, [user]);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8 dark:bg-gray-950">
@@ -15,10 +52,10 @@ const LoginPage = () => {
           <img
             alt="Logo"
             className="mx-auto h-12 w-auto"
-            height={500}
+            height={50}
             src="/logo.png"
             style={{
-              aspectRatio: "50/50",
+              aspectRatio: "1/1",
               objectFit: "cover",
             }}
             width={50}
@@ -32,11 +69,11 @@ const LoginPage = () => {
               className="font-medium text-indigo-600 hover:text-indigo-500 dark:text-indigo-500 dark:hover:text-indigo-400"
               href="/register"
             >
-               sign up for a new account
+              sign up for a new account
             </Link>
           </p>
         </div>
-        <form action="#" className="mt-8 space-y-6" method="POST">
+        <form className="mt-8 space-y-6">
           <input defaultValue="true" name="remember" type="hidden" />
           <div className="-space-y-px rounded-md shadow-sm">
             <div>
@@ -45,9 +82,10 @@ const LoginPage = () => {
               </Label>
               <Input
                 autoComplete="email"
-                className="relative block w-full appearance-none rounded-none rounded-t-md border  border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500  dark:bg-gray-800 dark:text-gray-50 dark:placeholder-gray-400 dark:border-gray-800"
+                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:bg-gray-800 dark:text-gray-50 dark:placeholder-gray-400 dark:border-gray-800"
                 id="email"
-                name="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
                 placeholder="Email address"
                 required
                 type="email"
@@ -59,9 +97,10 @@ const LoginPage = () => {
               </Label>
               <Input
                 autoComplete="current-password"
-                className="relative block w-full appearance-none rounded-none rounded-b-md border  border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500  dark:bg-gray-800 dark:text-gray-50 dark:placeholder-gray-400 dark:border-gray-800"
+                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:bg-gray-800 dark:text-gray-50 dark:placeholder-gray-400 dark:border-gray-800"
                 id="password"
-                name="password"
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
                 placeholder="Password"
                 required
                 type="password"
@@ -86,11 +125,12 @@ const LoginPage = () => {
           </div>
           <div>
             <Button
-              className="group relative flex w-full justify-center rounded-md border border-gray-200 border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-indigo-500 dark:hover:bg-indigo-600 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-950 dark:border-gray-800"
+               onClick={onLogin}
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-indigo-500 dark:hover:bg-indigo-600 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-950"
               type="submit"
               
             >
-              Sign in
+              {loading ? 'Signing in...' : 'Sign in'}
             </Button>
           </div>
         </form>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,97 +1,144 @@
-import React from 'react';
+"use client"
+import React, { useEffect } from 'react';
 import Link from "next/link"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
+import { Checkbox } from "@/components/ui/checkbox"
 import { Button } from "@/components/ui/button"
-import LoginPage from "@/app/login/page";
+import { useRouter } from 'next/navigation';
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const RegisterPage = () => {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8 dark:bg-gray-950">
-      <div className="w-full max-w-md space-y-8">
-        <div>
-          <img
-            alt="Logo"
-            className="mx-auto h-12 w-auto"
-            height={50}
-            src="/logo.png"
-            style={{
-              aspectRatio: "50/50",
-              objectFit: "cover",
-            }}
-            width={50}
-          />
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-50">
-            Create a new account
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600 dark:text-gray-400">
-            Or{' '}
-            <Link
-              className="font-medium text-indigo-600 hover:text-indigo-500 dark:text-indigo-500 dark:hover:text-indigo-400"
-              href="/login"
-            >
-              sign in to your account
-            </Link>
-          </p>
-        </div>
-        <form action="#" className="mt-8 space-y-6" method="POST">
-          <input defaultValue="true" name="remember" type="hidden" />
-          <div className="-space-y-px rounded-md shadow-sm">
-            <div>
-              <Label className="sr-only" htmlFor="name">
-                Name
-              </Label>
-              <Input
-                autoComplete="name"
-                className="relative block w-full appearance-none rounded-none rounded-t-md border  border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500  dark:bg-gray-800 dark:text-gray-50 dark:placeholder-gray-400 dark:border-gray-800"
-                id="name"
-                name="name"
-                placeholder="Name"
-                required
-                type="text"
-              />
-            </div>
-            <div>
-              <Label className="sr-only" htmlFor="email">
-                Email address
-              </Label>
-              <Input
-                autoComplete="email"
-                className="relative block w-full appearance-none rounded-none border  border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500  dark:bg-gray-800 dark:text-gray-50 dark:placeholder-gray-400 dark:border-gray-800"
-                id="email"
-                name="email"
-                placeholder="Email address"
-                required
-                type="email"
-              />
-            </div>
-            <div>
-              <Label className="sr-only" htmlFor="password">
-                Password
-              </Label>
-              <Input
-                autoComplete="new-password"
-                className="relative block w-full appearance-none rounded-none border  border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500  dark:bg-gray-800 dark:text-gray-50 dark:placeholder-gray-400 dark:border-gray-800"
-                id="password"
-                name="password"
-                placeholder="Password"
-                required
-                type="password"
-              />
-            </div>
-          </div>
-          <div>
-            <Button
-              className="group relative flex w-full justify-center rounded-md border border-gray-200 border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-indigo-500 dark:hover:bg-indigo-600 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-950 dark:border-gray-800"
-              type="submit"
-            >
-              Sign up
-            </Button>
-          </div>
-        </form>
-      </div>
-    </div>
-  )
-}
+  const router = useRouter();
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: "",
+    })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
+    const onSignup = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/register", user);
+            console.log("Signup success", response.data);
+            router.push("/login");
+            
+        } catch (error:any) {
+            console.log("Signup failed", error.message);
+            
+            toast.error(error.message);
+        }finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user]);
+
+
+
+
+
+
+
+    return (
+      <div className="flex min-h-screen items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8 dark:bg-gray-950">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <img
+              alt="Logo"
+              className="mx-auto h-12 w-auto"
+              height={50}
+              src="/logo.png"
+              style={{
+                aspectRatio: "50/50",
+                objectFit: "cover",
+              }}
+              width={50}
+            />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-50">
+            {loading ? "Processing" : "Signup a new account"}
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600 dark:text-gray-400">
+              Or{' '}
+              <Link
+                className="font-medium text-indigo-600 hover:text-indigo-500 dark:text-indigo-500 dark:hover:text-indigo-400"
+                href="/login"
+              >
+                sign in to your account
+              </Link>
+            </p>
+          </div>
+          <form action="#" className="mt-8 space-y-6" method="POST">
+            <input defaultValue="true" name="remember" type="hidden" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <Label className="sr-only" htmlFor="name">
+                  Name
+                </Label>
+                <Input
+                  autoComplete="name"
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border  border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500  dark:bg-gray-800 dark:text-gray-50 dark:placeholder-gray-400 dark:border-gray-800"
+                  id="name"
+                  value={user.username}
+                  onChange={(e) => setUser({...user, username: e.target.value})}
+                  placeholder="Name"
+                  required
+                  type="text"
+                />
+              </div>
+              <div>
+                <Label className="sr-only" htmlFor="email">
+                  Email address
+                </Label>
+                <Input
+                  autoComplete="email"
+                  className="relative block w-full appearance-none rounded-none border  border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500  dark:bg-gray-800 dark:text-gray-50 dark:placeholder-gray-400 dark:border-gray-800"
+                  id="email"
+                  value={user.email}
+                  onChange={(e) => setUser({...user, email: e.target.value})}
+                  placeholder="Email address"
+                  required
+                  type="email"
+                />
+              </div>
+              <div>
+                <Label className="sr-only" htmlFor="password">
+                  Password
+                </Label>
+                <Input
+                  autoComplete="new-password"
+                  className="relative block w-full appearance-none rounded-none border  border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500  dark:bg-gray-800 dark:text-gray-50 dark:placeholder-gray-400 dark:border-gray-800"
+                  id="password"
+                  value={user.password}
+                  onChange={(e) => setUser({...user, password: e.target.value})}
+                  placeholder="Password"
+                  required
+                  type="password"
+                />
+              </div>
+            </div>
+            <div>
+              <Button
+                onClick={onSignup}
+                className="group relative flex w-full justify-center rounded-md border border-gray-200 border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:bg-indigo-500 dark:hover:bg-indigo-600 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-950 dark:border-gray-800"
+                type="submit"
+              >
+                Sign up
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+  
 export default RegisterPage;
